64484a7dfebcc5bc446ba8dfc41835abcce6993f
daagomez <daagomez>
2026-07-01 17:39:22 -0500

    Task 2 - emotion_detection.py added

---
+++ emotion_detection.py
@@ -0,0 +1,46 @@
+import requests  # Import the requests library to handle HTTP requests
+import json  # to create a json from the response
+
+
+# Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+def sentiment_analyzer(text_to_analyse):
+    # URL of the sentiment analysis service
+    url = "https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
+    # Create a dictionary with the text to be analyzed
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    # Set the headers required for the API request
+    header = {
+        "grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"
+    }
+    # Send a POST request to the API with the text and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    # ---
+    # Step 4
+    # Formating the response as json
+    # formatted_response = json.loads(response.text)
+    # label = formatted_response["documentSentiment"]["label"]
+    # score = formatted_response["documentSentiment"]["score"]
+    # ---
+
+    # Step 7
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        label = formatted_response["documentSentiment"]["label"]
+        score = formatted_response["documentSentiment"]["score"]
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        label = None
+        score = None
+    # For any other unexpected status codes, set label and score to None
+    else:
+        label = None
+        score = None
+
+    # Return the response text from the API
+    # return {"label": label, "score": score}
+    #
+    # For the task 2 of the end proyect
+    return response.text
