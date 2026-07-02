0b5bfe41b451eef37ebebda2dcefeb3d3ac83eff
daagomez <daagomez>
2026-07-01 21:06:22 -0500

    Task 6: /emotionDetector

---
+++ server.py
@@ -0,0 +1,54 @@
+"""Executing this function initiates the application of sentiment
+analysis to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
+
+# Import Flask, render_template, request from the flask pramework package : TODO
+# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detection
+
+# Initiate the flask app : TODO
+app = Flask("Emotion Detector")
+
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    """This code receives the text from the HTML interface and
+    runs sentiment analysis over it using sentiment_analysis()
+    function. The output returned shows the label and its confidence
+    score for the provided text.
+    """
+    # TODO
+    text_to_analyze = request.args.get("textToAnalyze")
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detection(text_to_analyze)
+    # Extract the label and score from the response
+    # label = response["dominant_emotion"]
+    # Step 5
+    # ---
+    # Return a formatted string with the sentiment label and score
+    # return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['fear']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    # ---
+    # Check if the label is None, indicating an error or invalid input
+    if response is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the sentiment label and score
+        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['fear']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
+
+# @app.route("/")
+# def render_index_page():
+#     """This function initiates the rendering of the main application
+#     page over the Flask channel
+#     """
+#     # TODO
+#     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    """ This functions executes the flask app and deploys it on localhost:5000
+    """
+    # TODO
+    app.run(host="0.0.0.0", port=5000)
